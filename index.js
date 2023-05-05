ce794aa3efee8569f5c861ecfa95e29fe1c1f962
YuliyaSever <yuliya.severtokova@gmail.com>
2023-05-04 19:44:50 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+// 1
+const cleaningManSalary = 40000;
+const pedagogySalary = 22000;
+console.log(40000+22000)
+
+// 2
+let city;
+city = 'Владивосток';
+let n;
+n = 2000;
+console.log('Владивосток '+ "" + 2000);
+
+// 3
+const messengerTime = 128;
+const picturesTime = 254;
+const vkTime = 137;
+const youtubeTime = 201;
+console.log((128 + 254 + 137 + 201)/60);
+
+// 4
+let n1;
+n1 = 2;
+console.log(2 ** 5);
+
+// 5
+let messege;
+messege = '"Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!"';
+console.log(messege);
+
+// 6
+const greetings = 'Привет';
+const name = 'Кот';
+console.log( `"Привет, Кот"`);
+
+// 7
+let time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+let timeLess;
+timeLess = 15;
+console.log(time-timeLess);
+console.log('Немного сториз в соцсетях. Осталось минут: 19 ');
+let timeLessForNews;
+timeLessForNews = 10;
+console.log(timeLess-timeLessForNews);
+console.log( 'Немного не новостей, но событий. Осталось минут: 5');
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+
+// 8
+let fahrenheit;
+fahrenheit = 451;
+let celsius;
+console.log((fahrenheit - 32) / 1.8);
+celsius = 232.77777777777777;
+console.log('451 градуса по Фаренгейту - это 232.77777777777777  градуса по Цельсию.')
+
+// задача со звездочкой
+let deposit = 30000; 
+const bankRate = 0.185; 
+const growth = 150; 
+deposit = deposit + deposit * bankRate + growth;
+console.log("Ваш депозит на конец расчетного периода: " + deposit + " рублей");
+const rateAfterYear = 0.07;
+const depositAfterYear = deposit + deposit * rateAfterYear;
+console.log("Через год у вас будет " + depositAfterYear + " руб. на счету");
+
+
+
